7e50823e72ac81766e05d3ff790bc33bbe887ebd
Adam Craven <git@adamcraven.me>
2016-02-12 19:18:27 +0000

    Add touch events and format js

--- accordi-gallery.js
+++ accordi-gallery.js
@@ -1,23 +1,31 @@
 /*jshint unused:true*/
-(function() {
+(function () {
   d3.fisheye = {
-    scale: function(scaleType) {
+    scale: function (scaleType) {
       return d3_fisheye_scale(scaleType(), 3, 0);
     },
-    circular: function() {
+    circular: function () {
       var radius = 200,
-          distortion = 2,
-          k0,
-          k1,
-          focus = [0, 0];
+        distortion = 2,
+        k0,
+        k1,
+        focus = [0, 0];
 
       function fisheye(d) {
         var dx = d.x - focus[0],
-            dy = d.y - focus[1],
-            dd = Math.sqrt(dx * dx + dy * dy);
-        if (!dd || dd >= radius) return {x: d.x, y: d.y, z: dd >= radius ? 1 : 10};
+          dy = d.y - focus[1],
+          dd = Math.sqrt(dx * dx + dy * dy);
+        if (!dd || dd >= radius) return {
+          x: d.x,
+          y: d.y,
+          z: dd >= radius ? 1 : 10
+        };
         var k = k0 * (1 - Math.exp(-dd * k1)) / dd * .75 + .25;
-        return {x: focus[0] + dx * k, y: focus[1] + dy * k, z: Math.min(k, 10)};
+        return {
+          x: focus[0] + dx * k,
+          y: focus[1] + dy * k,
+          z: Math.min(k, 10)
+        };
       }
 
       function rescale() {
@@ -27,19 +35,19 @@
         return fisheye;
       }
 
-      fisheye.radius = function(_) {
+      fisheye.radius = function (_) {
         if (!arguments.length) return radius;
         radius = +_;
         return rescale();
       };
 
-      fisheye.distortion = function(_) {
+      fisheye.distortion = function (_) {
         if (!arguments.length) return distortion;
         distortion = +_;
         return rescale();
       };
 
-      fisheye.focus = function(_) {
+      fisheye.focus = function (_) {
         if (!arguments.length) return focus;
         focus = _;
         return fisheye;
@@ -53,28 +61,28 @@
 
     function fisheye(_) {
       var x = scale(_),
-          left = x < a,
-          range = d3.extent(scale.range()),
-          min = range[0],
-          max = range[1],
-          m = left ? a - min : max - a;
+        left = x < a,
+        range = d3.extent(scale.range()),
+        min = range[0],
+        max = range[1],
+        m = left ? a - min : max - a;
       if (m == 0) m = max - min;
       return (left ? -1 : 1) * m * (d + 1) / (d + (m / Math.abs(x - a))) + a;
     }
 
-    fisheye.distortion = function(_) {
+    fisheye.distortion = function (_) {
       if (!arguments.length) return d;
       d = +_;
       return fisheye;
     };
 
-    fisheye.focus = function(_) {
+    fisheye.focus = function (_) {
       if (!arguments.length) return a;
       a = +_;
       return fisheye;
     };
 
-    fisheye.copy = function() {
+    fisheye.copy = function () {
       return d3_fisheye_scale(scale.copy(), d, a);
     };
 
@@ -87,16 +95,11 @@
 
 var accordiGallers = d3.selectAll('[data-accordi-gallers]');
 
-
-
-
 accordiGallers.each(createGallers);
 
 function createGallers() {
   var ctx = d3.select(this);
   var data = JSON.parse(ctx.attr('collection-info'));
-
-
   var width = ctx.node().getBoundingClientRect().width;
   var easing = d3.ease('cubic');
   var inContext = false; // Are we hovering/focused on the item?
@@ -108,20 +111,62 @@ function createGallers() {
     .distortion(0)
     .focus(width / 2);
 
+  function mousemove() {
+    if (inContext === false) {
+      grow();
+    }
+    inContext = true;
+    var mousePos = d3.mouse(this)[0];
+    mousePos = (mousePos < x.range()[0]) ? x.range()[0] : mousePos
+    mousePos = (mousePos > x.range()[1]) ? x.range()[1] : mousePos
+
+    x.focus(mousePos);
+    render();
+  }
+
+  function mouseout() {
+    if (inContext === true) {
+      shrink();
+    }
+    inContext = false;
+  }
+
+  var touchstart;
+
   ctx
-    .on('mousemove', function mousemove() {
-      if (inContext === false) {
-        grow();
+    .on('mousemove', mousemove)
+    .on('mouseleave', mouseout)
+    .on('touchend', function () {
+      console.log('end', d3.event.touches);
+
+      var isQuickClick = Date.now() - touchstart < 500;
+      var isNotMovedClick = touchstartPos === d3.mouse(this)[0]
+
+      if (isQuickClick || isNotMovedClick) {
+        alert('click');
       }
-      inContext = true;
-      x.focus(d3.mouse(this)[0]);
-      render();
+
+      mouseout()
     })
-    .on('mouseleave', function mouseout() {
-      if(inContext === true) {
-        shrink();
+    .on('touchmove', function () {
+      if (d3.event.touches.length === 1) {
+
+        mousemove.call(this)
       }
-      inContext = false;
+    })
+    .on('touchstart', function () {
+      d3.event.preventDefault();
+
+
+      if (d3.event.touches.length === 1) {
+        inContext = false;
+
+        console.log(d3.event.touches)
+
+        mousemove.call(this);
+        touchstartPos = d3.mouse(this)[0];
+        touchstart = Date.now();
+      }
     });
 
 
@@ -169,26 +214,32 @@ function createGallers() {
       });
   }
 
-  ctx.selectAll('ul li')
+  var liEl = ctx.selectAll('ul li')
     .data(data)
     .enter()
     .append('li')
-    .style('position', 'absolute')
-    .style('top', '0')
     .style('background-image', function (d) {
-      return 'url('+ d.path + ')';
+      return 'url(' + d.path + ')';
     })
-    .style('outline', '1px solid black')
-    .style('background-size', '200px')
-    .style('background-repeat', 'no-repeat')
-    .style('background-position', 'center')
-    .style('float', 'left')
-    .append('a')
-    .attr('href', 'http://my-add.com')
     .style({
-      width:'100%',
+      'position': 'absolute',
+      'top': '0',
+      'outline': '1px solid black',
+      'background-size': '200px',
+      'background-repeat': 'no-repeat',
+      'background-position': 'center',
+      height: '300px',
+      'float': 'left'
+    })
+
+  var aEl = liEl.append('a')
+    .attr('href', function (d) {
+      return d.href;
+    })
+    .style({
+      width: '100%',
       height: '100%',
-      display:'block'
+      display: 'block'
     })
 
   render();
